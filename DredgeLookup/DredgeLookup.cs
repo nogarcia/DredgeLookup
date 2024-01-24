cf90d7f02dddd7f504d75fca3a8cc913da0b5281
Noah García <noah@danielgarcia.info>
2024-01-24 01:43:45 -0600

    1.0.0: MVP

---
+++ DredgeLookup/DredgeLookup.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace DredgeLookup
+{
+	public class DredgeLookup : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(DredgeLookup)} has loaded!");
+		}
+
+		public void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.L)
+				&& GameManager.Instance // Make sure the game is loaded!
+				&& GameManager.Instance.GridManager // ...make sure the GridManager is loaded too
+				&& !GameManager.Instance.GridManager.IsCurrentlyHoldingObject() // The game won't let you open Encyclopedia while holding an object anyway
+				&& GameManager.Instance.GridManager.CurrentlyHoveredObject.ItemData is FishItemData)
+			{
+				FishItemData fishItemData = GameManager.Instance.GridManager.currentlyHoveredObject.ItemData as FishItemData;
+				Encyclopedia encyclopedia = GameObject.Find("/GameCanvases/PopupCanvas/EncyclopediaWindow/Container/Encyclopedia").GetComponent<Encyclopedia>();
+
+				if (encyclopedia.currentFishList == null)
+				{
+					// currentFishList is first set in Encyclopedia's Awake(), which potentially hasn't been called yet if this is the first
+					// time the user's opened the encyclopedia in the session.
+
+					// In Awake(), it's immediately set to Encyclopedia.allFish, so we just do this early the first time.
+					encyclopedia.currentFishList = encyclopedia.allFish;
+				}
+
+				int fishIndex = encyclopedia.currentFishList.FindIndex(fish => fish == fishItemData);
+
+				if (fishIndex == -1)
+				{
+					// Either this fish doesn't exist, or (more likely) the encyclopedia filters exclude it.
+					return;
+				}
+
+				encyclopedia.currentIndex = fishIndex;
+				encyclopedia.RefreshUI();
+			}
+		}
+	}
+}
---
+++ DredgeLookup/Loader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DredgeLookup
+{
+	public class Loader
+	{
+		/// <summary>
+		/// Run by Winch to initialize the mod
+		/// </summary>
+		public static void Initialize()
+		{
+			var gameObject = new GameObject(nameof(DredgeLookup));
+			gameObject.AddComponent<DredgeLookup>();
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
